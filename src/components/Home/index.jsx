4c1f9c852a596cced465975c980f73a8667fced9
Charles Kamiri <78848071+kamiri-charles@users.noreply.github.com>
2023-03-11 19:59:29 +0300

    Bug fix

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,16 +1,8 @@
-import { useEffect, useState } from "react"
 import { useNavigate } from 'react-router-dom' 
 import './styles.scss'
 
 const Home = () => {
-
-    const [user, setUser] = useState()
     let nav = useNavigate()
-
-    useEffect(() => {
-        let user = localStorage.getItem('user')
-        user ? setUser(user) : nav('/sign-in')
-    }, [nav])
 
     return (
         <div className="home">
